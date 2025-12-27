3b3ceebb49baefe7c1a039ef205bf5d7e87bdb27
Rakesh Karmaker <rakeshkarmaker0175@gmail.com>
2025-12-28 02:08:04 +0600

    build: change the file location

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
 import { router } from "./router/router";
-
-import "./index.css";
 import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import { HelmetProvider } from "react-helmet-async";
+
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <HelmetProvider>
